b0b12320c16acbaef0a6ec36c80e5bff353246c3
김가현 <gahyun3722@gmail.com>
2024-03-05 09:47:36 +0900

    2차 수정

--- javascript/visit.js
+++ javascript/visit.js
@@ -1,15 +1,20 @@
-$('.map-menu li a').click(function(e){
+$('.map-menu li').click(function (e) {
     e.preventDefault()
-    let href=$(this).attr('href')
-
-    $('.map-container li').removeClass('on')
-    $(href).addClass('on')
-    
-    $('.map-menu li a').removeClass('on')
+    let idx = $(this).index()
+    $('.map-menu li').removeClass('on')
     $(this).addClass('on')
+
+    $('.map-img').removeClass('on')
+    $('.map-img').eq(idx).addClass('on')
+
+    $('.exhibitions').removeClass('on')
+    $('.exhibitions').eq(idx).addClass('on')
+    
+   
+    $('.video li').removeClass('on')
+    $('.video li').eq(idx).addClass('on')
 })
 
-
-
-
-
+$('a.close').click(function(){
+    $('.video li').fadeOut()
+})
